251c718af5b9f2ab96fe357955f9b44bf2b56058
Tim Richards <pdxorsunsets@gmail.com>
2022-04-15 22:10:41 -0400

    basic data models implemented

---
+++ WorkoutManager/WorkoutManager/WorkoutManager/Models/Set.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkoutManager.Models
+{
+    public class Set
+    {
+        public int Weight { get; set; }
+
+        public int Repetitions { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public bool Completed { get; set; }
+
+        public string Id { get; set; }
+    }
+}
