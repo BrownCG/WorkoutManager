23c1950489d3e6de74dd42f846b58aeac44b3ca7
Tim Richards <pdxorsunsets@gmail.com>
2022-04-16 18:14:54 -0400

    AddNewExercise Page Route outline implemented

---
+++ WorkoutManager/WorkoutManager/WorkoutManager/ViewModels/NewExerciseViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using WorkoutManager.Models;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WorkoutManager.ViewModels
+{
+    public class NewExerciseViewModel : BaseViewModel
+    {
+        private string name;
+
+        private Set[] sets;
+
+        public NewExerciseViewModel()
+        {
+            SaveCommand = new Command(OnSave);
+            CancelCommand = new Command(OnCancel);
+        }
+
+        public Command SaveCommand { get; }
+        public Command CancelCommand { get; }
+
+        public string Name
+        {
+            get => name;
+            set => SetProperty(ref name, value);
+        }
+
+        #region Commands
+        private async void OnSave()
+        {
+            Exercise newExercise = new Exercise()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Name,
+                Sets = new Set[] { new Set() { Repetitions = 1, Weight = 50 } }
+            };
+
+            await DataStore.AddExerciseAsync(newExercise);
+
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void OnCancel()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+        #endregion
+    }
+}
